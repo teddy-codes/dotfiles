4a978946e887f77f1a3575ae326166b38091d2d4
Robert Miller <23535075+teddy-codes@users.noreply.github.com>
2025-02-25 23:05:32 -0500

    vim: remove dashboard - replace with file finder

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,2 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.api.nvim_create_autocmd("UIEnter", {
+  once = true,
+  callback = function()
+    if vim.fn.argc() == 0 then
+      -- Close any remaining dashboard buffer
+      vim.cmd("silent! bdelete")
+      -- Open file finder
+      require("telescope.builtin").find_files()
+    end
+  end,
+})
