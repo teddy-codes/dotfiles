42ad7c6560f9b1e8b2cc4431d1d4fcd7905d7ad7
Robert <23535075+teddy-codes@users.noreply.github.com>
2025-08-16 14:38:24 -0400

    remove unused config

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,24 +1,3 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
-local opt = vim.opt
-
--- Editor behavior
-opt.relativenumber = true
-opt.number = true
-opt.clipboard = "unnamedplus"
-opt.conceallevel = 0 -- Show markdown markup, useful for terraform/yaml files
-opt.linebreak = true
-opt.wrap = false
-
--- Indentation (important for YAML)
-opt.tabstop = 2
-opt.shiftwidth = 2
-opt.expandtab = true
-opt.smartindent = true
-
--- Search
 opt.ignorecase = true
 opt.smartcase = true
 opt.hlsearch = true
